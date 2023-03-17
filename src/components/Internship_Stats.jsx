282d62570b397c5663f592a11c791d9089aea560
Rohit <prjagtap90@gmail.com>
2023-03-17 12:47:20 +0530

    need to add small graphs

--- src/components/Internship_Stats.jsx
+++ src/components/Internship_Stats.jsx
@@ -1,42 +1,60 @@
-import React from 'react'
-import {Typography,Box,Grid,Paper} from "@mui/material";
-
-
-
+import React from "react";
+import { Typography, Box, Grid, Paper, Card, Stack } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
 export default function Internship_Stats() {
   return (
     <>
-    <Box className='paddingall mb'>
-        <Grid container
-        style={{
-          display:'flex',
-          alignItems:'center',
-          justifyContent:'space-around'
-        }}
-        spacing={1}
-        >
-            <Grid Item md={6} xs={12} className='mb'>
-            <Paper className='ml' style={{padding:'30px 50px'}}>
-              <Typography variant='h6'>Total Internship Hosted - 5</Typography>
-              <Typography variant='h6'>Total Applicants - 200</Typography>
-              <Typography variant='h6'>A+ Grade Submission- 10</Typography>
-              <Typography variant='h6'>A Grade Submission - 30</Typography>
-              <Typography variant='h6'>B,C Grade Submission - 160</Typography>
-              </Paper>
-            </Grid>
-            <Grid Item md={6} xs={12} className='mb'>
-            <Paper className='ml' style={{padding:'10px 50px'}}>
-            <Typography variant='h4'>Rewards And Recognition</Typography>
-            <Typography variant='h6'>Immersion hosted - 10</Typography>
-              <Typography variant='h6'> Monetary reward - 10</Typography>
-              <Typography variant='h6'>Pending Immersions - 20 </Typography>
-              <Typography variant='h6'>Onsite Internships awarded - 5 </Typography>
-              <Typography variant='h6'>Job Offers made - 5</Typography>
-              </Paper>
-            </Grid>
+      <Box className="paddingall mb">
+        <Grid container spacing={1} className="mb">
+          <Grid xs={6} md={3}>
+            <Paper style={{ textAlign: "center" }}>
+              <Typography variant="h6">Internship Hosted</Typography>
+              <Typography variant="h4">5</Typography>
+            </Paper>
+          </Grid>
+          <Grid xs={6} md={3} s>
+            <Paper style={{ textAlign: "center" }}>
+              <Typography variant="h6">Total Applicants</Typography>
+              <Typography variant="h4">20</Typography>
+            </Paper>
+          </Grid>
+          <Grid xs={6} md={3}>
+            <Paper style={{ textAlign: "center" }}>
+              <Typography variant="h6">Immersion Hosted</Typography>
+              <Typography variant="h4">5</Typography>
+            </Paper>
+          </Grid>
+          <Grid xs={6} md={3}>
+            <Paper style={{ textAlign: "center" }}>
+              <Typography variant="h6">Job Offer Made</Typography>
+              <Typography variant="h4">5</Typography>
+            </Paper>
+          </Grid>
         </Grid>
-    </Box>
+        <Grid container spacing={0}>
+          <Grid xs={6}>
+            <Paper style={{ textAlign: "center" }}>
+              <Typography variant="h6">A+ Grade Submission- 10</Typography>
+            </Paper>
+          </Grid>
+          <Grid xs={6}>
+            <Paper style={{ textAlign: "center" }}>
+              <Typography variant="h6">A Grade Submission - 30</Typography>
+            </Paper>
+          </Grid>
+          <Grid xs={6}>
+            <Paper style={{ textAlign: "center" }}>
+              <Typography variant="h6">B,C Grade Submission - 160</Typography>
+            </Paper>
+          </Grid>
+          <Grid xs={6}>
+            <Paper style={{ textAlign: "center" }}>
+              <Typography variant="h6">Pending Immersions - 20 </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Box>
     </>
-  )
+  );
 }
